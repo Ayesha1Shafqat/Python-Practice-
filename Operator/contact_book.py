4153b11f0432ec544ab328fe6303942ec3292d16
Ayesha Shafqat <ayeshashafqat507@gmail.com>
2025-08-17 23:39:43 +0500

    Add files via upload

---
+++ Operator/contact_book.py
@@ -0,0 +1,11 @@
+# Contact details stored in a dictionary
+contact = {
+    "Name": "Ayesha",
+    "Phone": "0300-1234567",
+    "Email": "ayesha@example.com"
+}
+
+# Printing the details
+print("Name:", contact["Name"])
+print("Phone:", contact["Phone"])
+print("Email:", contact["Email"])
